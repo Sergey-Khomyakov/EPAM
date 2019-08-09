6fb2a9c0de004a85351ff4cce5d8cde42ae7dfbd
Сергей Хомяков <ser.xomiakoff@yandex.ru>
2019-08-09 12:32:59 +0400

    Add Task_1

---
+++ Task_1/Task_1/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Задание 1. Сортировка
+    Дан массив целых чисел. Напишите функцию, которая получает данный массив в 
+    качестве аргумента и сортирует его по возрастанию, а также программу для демонстрации работы этой функции.
+ */
+using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        private static int[] Sort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int position = array[i];
+                int j = i - 1;
+                while (j >= 0 && array[j] > position)
+                {
+                    array[j + 1] = array[j];
+                    j -= 1;
+                }
+                array[j + 1] = position;
+            }
+            return array;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 5, 2, 4, 12, 1, 3, 6, 7, 8, 9, 11, 15, 14, 13, 10, 16, 20, 17, 19, 18 };
+            Console.Write("Изначальный массив: ");
+            foreach (var number in array)
+            {
+                Console.Write(number);
+            }
+            array = Sort(array);
+            Console.Write("\nОтсортированный массив: ");
+            foreach (var number in array)
+            {
+                Console.Write(number);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
