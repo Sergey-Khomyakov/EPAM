2baeefc22f6d07cfde31820b2b32b33b3614e17c
Сергей Хомяков <ser.xomiakoff@yandex.ru>
2019-08-09 12:38:24 +0400

    Add Task_4

---
+++ Task_4/Task_4/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Задание 4 Факториал
+    Написать функцию нахождения факториала числа n.
+ */
+using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        private static int Function(int n)
+        {
+            int Sum = 1;
+            for (int i = n; i > 1; i--)
+            {
+                Sum = Sum * i;
+            }
+            return Sum;
+        }
+        static void Main(string[] args)
+        {
+            int t = Function(5);
+            Console.WriteLine(t);
+            Console.ReadKey();
+        }
+    }
+}
